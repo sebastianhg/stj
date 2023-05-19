19131b341c90a6c83bd0b4152c6374d1bdcc32c2
sebastian hincapie <86570008+sebastianhg@users.noreply.github.com>
2023-05-19 13:54:52 -0500

    Update server2.js

--- project/server2.js
+++ project/server2.js
@@ -1,84 +1,19 @@
-// const express = require('express');
-// const app = express();
-
-// // Ruta para el formulario
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/formulario.html');
-// });
-
-// // Ruta para realizar el scraping
-// app.get('/scrape', (req, res) => {
-//     // Requerir y ejecutar el archivo de scraping
-//     require('./scraping.js')();
-//     res.send('Scraping en progreso.'); // O cualquier otro mensaje de respuesta
-// });
-
-// // Iniciar el servidor en el puerto 3000
-// app.listen(3000, () => {
-//     console.log('Servidor escuchando en el puerto 3000');
-// });
-
-
-
-
-// const express = require('express');
-// const hacerBusqueda = require('./scraping.js'); // Importa la función hacerBusqueda desde scraping.js
-// const app = express();
-
-// // Ruta para el formulario
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/formulario.html');
-// });
-
-// // // Ruta para realizar el scraping
-// // app.get('/scrape', (req, res) => {
-// //     // Llama a la función hacerBusqueda()
-// //     hacerBusqueda();
-// //     res.send('Scraping en progreso.'); // O cualquier otro mensaje de respuesta
-// // });
-
-
-// app.get('/scrape', async (req, res) => {
-//     // Requerir y ejecutar el archivo de scraping
-//     const coinciden = await require('./scraping.js')();
-//     let mensaje = 'El número de expediente y año no coinciden en la tabla: no coincide';
-//     if (coinciden) {
-//         mensaje = 'El número de expediente y año coinciden en la tabla: coincide';
-//     }
-//     res.send(mensaje);
-// });
-
-
-// // Iniciar el servidor en el puerto 3000
-// app.listen(3000, () => {
-//     console.log('Servidor escuchando en el puerto 3000');
-// });
-
-
-
-
 const express = require('express');
-const hacerBusqueda = require('./scraping.js'); // Importa la función hacerBusqueda desde scraping.js
+const GetExpedientes = require('./mapper.js'); 
 const app = express();
 
-// Ruta para el formulario
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
     res.sendFile(__dirname + '/formulario.html');
 });
 
-// Ruta para realizar el scraping
-app.get('/scrape', async (req, res) => {
+app.get('/get-expedientes', async (req, res) => {
     const numeroExpediente = req.query.asunto;
-    const anio = req.query.anio; // Obtener el valor del parámetro anio de la URL
-
-    // Llamar a la función hacerBusqueda con los valores proporcionados
-    const resultado = await hacerBusqueda(numeroExpediente, anio);
-
-    // Enviar la respuesta como JSON con el resultado del scraping
-    res.json({ resultado });
+    const anio = req.query.anio;
+    const titular = req.query.nombre;
+    const resultado = await GetExpedientes(titular, numeroExpediente, anio)
+    res.json({"response": resultado});
 });
 
-// Iniciar el servidor en el puerto 3000
 app.listen(3000, () => {
     console.log('Servidor escuchando en el puerto 3000');
 });
