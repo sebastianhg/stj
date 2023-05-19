ff2f7be6cf6aeed4880c90ac3a9768305ca469ff
sebastian hincapie <86570008+sebastianhg@users.noreply.github.com>
2023-05-19 13:56:33 -0500

    Update mapper.js

--- project/mapper.js
+++ project/mapper.js
@@ -1,1 +1,76 @@
+const {juzgados, anios} = require("./definiciones.js")
+const HacerBusqueda = require("./scraping.js")
 
+async function GetExpedientes(titular, numeroExpediente, anio) {
+    let response = []
+    for (let i = 0; i < juzgados.length; i++) {
+        const j = juzgados[i];
+        let res = await expedientes(j, titular, numeroExpediente, anio)
+        response.concat(res) 
+    }
+}
+
+async function expedientes(juzgado, titular, numeroExpediente, anio) {
+    let validAnios = (anio == null || anio == "")
+    let validAsunto = (numeroExpediente == null || numeroExpediente == "")
+    
+    if (validAnios  && validAsunto) {
+        return getExpedientesByAnioAndExpediente(juzgado, titular)
+    }
+    
+    if (validAnios) {
+        return executeByAnios(juzgado, titular, numeroExpediente)
+    }
+    
+    if (validAsunto) {
+        return executeByExpedientes(juzgado, titular, anio)
+    }
+
+    return executeWithAllInfo(juzgado, titular, numeroExpediente, anio)
+}   
+
+async function executeWithAllInfo(juzgado, titular, numeroExpediente, anio) {
+    let res = []
+    let response = await HacerBusqueda(juzgado, titular, numeroExpediente, anio)
+    res = res.concat(response)
+    return res
+}
+
+async function getExpedientesByAnioAndExpediente(juzgado, titular) {
+    let res = []
+    for (let index = 0; index < anios.length; index++) {
+        for (let i = 1; i < 9999; i++) {
+            let a = i.toString().padStart(4, "0");
+            if (a.length == 4) {
+                let response = await HacerBusqueda(juzgado, titular, a, anios[index])
+                let data = await response.json();
+                res = res.concat(data.resultado);
+            }
+        }
+    }
+    return res
+}
+
+async function executeByAnios(juzgado, titular, numeroExpediente) {
+    let res = []
+    for (let index = 0; index < anios.length; index++) {
+        n = anios[index]
+        let response = await HacerBusqueda(juzgado, titular, numeroExpediente,  n)
+        res = res.concat(response)
+    }
+    return res
+}
+
+async function executeByExpedientes(juzgado, titular, anio) {
+    let res = []
+    for (let i = 1; i < 9999; i++) {
+        let a = i.toString().padStart(4, "0")
+        if(a.length == 4) {
+            let response = await HacerBusqueda(juzgado, titular, a, anio)
+            res = res.concat(response)
+        }
+    }
+    return res
+}
+
+module.exports = GetExpedientes
